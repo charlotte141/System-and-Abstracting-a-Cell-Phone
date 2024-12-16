9a5b5f1d868dc6bd3f8b7e1253e2551f09fd6cc2
Delcio_Santos <87926383+charlotte141@users.noreply.github.com>
2024-12-16 14:34:49 +0100

    Initial project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("12345", "Modelo 1", "11111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("56789", "Modelo 2", "2222222", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
